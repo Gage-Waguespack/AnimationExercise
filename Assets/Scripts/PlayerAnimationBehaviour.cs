f289e6675268d81a5bb8e2562d0cc3ac8adb0cee
Gage <gagewaguespack@gmail.com>
2021-05-28 11:58:22 -0500

    Followed class example (just completed CheckIfEven)

---
+++ Assets/Scripts/PlayerAnimationBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationBehaviour : MonoBehaviour
+{
+    private PlayerMovementBehaviour _movementBehaviour;
+    [SerializeField]
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initialize the move component reference
+        _movementBehaviour = GetComponent<PlayerMovementBehaviour>();
+    }
+
+    public void ActivateWinTrigger()
+    {
+        _animator.SetTrigger("WinGame");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _animator.SetFloat("Speed", _movementBehaviour.Velocity.magnitude);
+        _animator.SetBool("InAir", !_movementBehaviour.IsGrounded);
+    }
+}
